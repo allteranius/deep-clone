296c6be7de7bbb1b0a63acd2761cae52ece845d4
allteranius <allteranius@yandex.ru>
2024-04-23 13:54:34 +0300

    Clean code

--- src/main/java/demo/deepclone/CopyUtils.java
+++ src/main/java/demo/deepclone/CopyUtils.java
@@ -21,71 +21,26 @@ public class CopyUtils {
         if (cached != null) {
             return cached;
         }
-        if (obj.getClass().isPrimitive()
-                || obj.getClass().isEnum()
-                || obj instanceof Number
-                || obj instanceof Character
-                || obj instanceof String
-                || obj instanceof Boolean) {
+        if (isImmutable(obj)) {
             return obj; // No need to copy immutable objects or primitive type
         }
         if (obj.getClass().isArray()) {
-            var size = Array.getLength(obj);
-            var copyArray = Array.newInstance(obj.getClass().getComponentType(), size);
-            for (int i = 0; i < size; i++) {
-                var element = Array.get(obj, i);
-                var elementCopy = deepCopy(element, cloned);
-                Array.set(copyArray, i, elementCopy);
-            }
-            return (T) copyArray;
+            return copyArray(obj, cloned);
         }
         if (obj instanceof Collection<?>) {
-            var collections = (Collection) obj;
-            Collection collectionsCopy = null;
-            try {
-                collectionsCopy =  createInstance(collections, null);
-            } catch (Exception e){
-                if(collections instanceof List<?>){
-                    collectionsCopy = new ArrayList();
-                } else if(collections instanceof Set){
-                    collectionsCopy = new HashSet();
-                } else {
-                    throw e;
-                }
-            }
-            for (Object element : collections) {
-                var elementCopy = deepCopy(element, cloned);
-                collectionsCopy.add(elementCopy);
-            }
-            return (T) collectionsCopy;
+            return copyCollections((Collection) obj, cloned);
         }
         if (obj instanceof Map) {
-            var map = (Map) obj;
-
-            Map copyMap = null;
-            try{
-                copyMap = createInstance(map, null);
-            } catch (Exception e){
-                copyMap = new HashMap();
-            }
-            Set<Map.Entry> set = map.entrySet();
-            for (Map.Entry entry : set) {
-                var keyCopy = deepCopy(entry.getKey(), cloned);
-                var valueCopy = deepCopy(entry.getValue(), cloned);
-                copyMap.put(keyCopy, valueCopy);
-            }
-            return (T) copyMap;
+            return copyMap((Map) obj, cloned);
         }
+        var fieldsData = getFiledData(obj);
+        T out = createInstance(obj, cloned, fieldsData.fields);
+        copyFields(obj, cloned, fieldsData.fields, out);
+        fieldsData.returnAccessFlag();
+        return out;
+    }
 
-        Constructor constructor = null;
-        T out = null;
-        try {
-            constructor = obj.getClass().getConstructors()[0];
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new NoSuitableConstructorException("Unable to create instance of "
-                    + obj.getClass() + " reason " + e.getMessage());
-        }
+    private static FiledData getFiledData(Object obj) {
         var fields = Arrays.stream(obj.getClass().getDeclaredFields())
                 .filter(field -> !(Modifier.isStatic(field.getModifiers()) || Modifier.isTransient(field.getModifiers())))
                 .collect(Collectors.toList());
@@ -94,39 +49,10 @@ public class CopyUtils {
             fieldAccessMap.put(field, field.canAccess(obj));
             field.setAccessible(true);
         }
-        var parameters = constructor.getParameters();
-        var args = new Object[parameters.length];
-        if (parameters.length > 0) {
-            for (int i = 0; i < parameters.length; i++) {
-                var parameter = parameters[i];
-                var field = fields.stream()
-                        .filter(x -> parameter.getType().isAssignableFrom(x.getType()))
-                        .findFirst();
-                if (field.isPresent()) {
-                    try {
-                        var copy = deepCopy(field.get().get(obj), cloned);
-                        args[i] = copy;
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException(" Error while get variable " + e.getMessage());
-                    }
-                    fields.remove(field.get());
-                }
-            }
-        }
-        try {
-            if (args.length > 0) {
-                out = (T) constructor.newInstance(args);
-            } else {
-                out = (T) constructor.newInstance();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new NoSuitableConstructorException("Unable to create instance of "
-                    + obj.getClass() + " reason " + e.getMessage());
-        }
+        return new FiledData(fields, fieldAccessMap);
+    }
 
-        cloned.computeIfAbsent(obj.hashCode(), x -> new ArrayList<>()).add(new Cloned(obj, out));
-
+    private static <T> void copyFields(T obj, Map<Integer, List<Cloned>> cloned, List<Field> fields, T out) {
         for (Field field : fields) {
             try {
                 field.set(out, deepCopy(field.get(obj), cloned));
@@ -134,10 +60,102 @@ public class CopyUtils {
                 throw new RuntimeException(" Error while get variable " + e.getMessage());
             }
         }
-        for (Map.Entry<Field, Boolean> entry : fieldAccessMap.entrySet()) {
-            entry.getKey().setAccessible(entry.getValue());
+    }
+
+    private static <T> T createInstance(T obj, Map<Integer, List<Cloned>> cloned, List<Field> fields) {
+        try {
+            T out;
+            Constructor constructor = obj.getClass().getConstructors()[0];
+            var parameters = constructor.getParameters();
+            if (parameters.length > 0) {
+                var args = new Object[parameters.length];
+                for (int i = 0; i < parameters.length; i++) {
+                    findAndCopyArgs(obj, cloned, fields, parameters, i, args);
+                }
+                out = (T) constructor.newInstance(args);
+            } else {
+                out = (T) constructor.newInstance();
+            }
+            cloned.computeIfAbsent(obj.hashCode(), x -> new ArrayList<>()).add(new Cloned(obj, out));
+            return out;
+        } catch (Exception e) {
+            throw new NoSuitableConstructorException("Unable to create instance of "
+                    + obj.getClass() + " reason " + e.getMessage());
         }
-        return out;
+
+    }
+
+    private static <T> void findAndCopyArgs(T obj, Map<Integer, List<Cloned>> cloned, List<Field> fields, Parameter[] parameters, int i, Object[] args) {
+        var parameter = parameters[i];
+        var field = fields.stream()
+                .filter(x -> parameter.getType().isAssignableFrom(x.getType()))
+                .findFirst();
+        if (field.isPresent()) {
+            try {
+                var copy = deepCopy(field.get().get(obj), cloned);
+                args[i] = copy;
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(" Error while get variable " + e.getMessage());
+            }
+            fields.remove(field.get());
+        }
+    }
+
+    private static <T> T copyMap(Map obj, Map<Integer, List<Cloned>> cloned) {
+        var map = obj;
+        Map copyMap = null;
+        try {
+            copyMap = createInstance(map);
+        } catch (Exception e) {
+            copyMap = new HashMap();
+        }
+        Set<Map.Entry> set = map.entrySet();
+        for (Map.Entry entry : set) {
+            var keyCopy = deepCopy(entry.getKey(), cloned);
+            var valueCopy = deepCopy(entry.getValue(), cloned);
+            copyMap.put(keyCopy, valueCopy);
+        }
+        return (T) copyMap;
+    }
+
+    private static <T> T copyCollections(Collection collections, Map<Integer, List<Cloned>> cloned) {
+        Collection collectionsCopy;
+        try {
+            collectionsCopy = createInstance(collections);
+        } catch (Exception e) {
+            if (collections instanceof List<?>) {
+                collectionsCopy = new ArrayList();
+            } else if (collections instanceof Set) {
+                collectionsCopy = new HashSet();
+            } else {
+                throw e;
+            }
+        }
+        for (Object element : collections) {
+            var elementCopy = deepCopy(element, cloned);
+            collectionsCopy.add(elementCopy);
+        }
+        return (T) collectionsCopy;
+    }
+
+    private static <T> T copyArray(T obj, Map<Integer, List<Cloned>> cloned) {
+        var size = Array.getLength(obj);
+        var copyArray = Array.newInstance(obj.getClass().getComponentType(), size);
+        for (int i = 0; i < size; i++) {
+            var element = Array.get(obj, i);
+            var elementCopy = deepCopy(element, cloned);
+            Array.set(copyArray, i, elementCopy);
+        }
+        return (T) copyArray;
+    }
+
+    private static <T> boolean isImmutable(T obj) {
+        return obj.getClass().isPrimitive()
+                || obj.getClass().isEnum()
+                || obj instanceof Number
+                || obj instanceof Character
+                || obj instanceof String
+                || obj instanceof Boolean;
     }
 
     private static <T> T checkObjectInCache(T obj, Map<Integer, List<Cloned>> cloned) {
@@ -153,11 +171,8 @@ public class CopyUtils {
         return null;
     }
 
-    private static <T> T createInstance(T obj, Object[] arg) {
+    private static <T> T createInstance(T obj) {
         try {
-            if (arg != null && arg.length > 0) {
-                return (T) obj.getClass().getDeclaredConstructor().newInstance(arg);
-            }
             return (T) obj.getClass().getDeclaredConstructor().newInstance();
         } catch (Exception e) {
             throw new NoSuitableConstructorException("Unable to create instance of "
@@ -167,5 +182,13 @@ public class CopyUtils {
     }
 
     private record Cloned(Object origin, Object cloned) {
+    }
+
+    private record FiledData(List<Field> fields, HashMap<Field, Boolean> fieldAccessMap) {
+        public void returnAccessFlag() {
+            for (Map.Entry<Field, Boolean> entry : fieldAccessMap.entrySet()) {
+                entry.getKey().setAccessible(entry.getValue());
+            }
+        }
     }
 }
